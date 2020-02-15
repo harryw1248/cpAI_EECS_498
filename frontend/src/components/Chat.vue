7f670765477a649fd87549a0b70c693b08c462b1
koeul <koeul@umich.edu>
2020-02-15 14:24:05 -0500

    chat interface implemented - work in progress

---
+++ frontend/src/components/Chat.vue
@@ -0,0 +1,141 @@
+<template>
+    <div class="w-full h-screen">
+        Chat History
+        <div v-if="this.debugModal">
+          asdfas
+        </div>
+        <section id="chat" class="overflow-y-auto py-4 px-2 mt-2 rounded">
+            <div
+                v-for="(message, index) in messages"
+                v-bind:key="index"
+                class="flex flex-col mt-4"
+            >
+                <div v-if="message['who']==='You'"
+                    class="flex flex-col py-1 items-start pl-4 w-8/12 bg-gray-100 rounded">
+                    <p class="font-semibold">
+                        {{ message['who'] }}
+                    </p>
+                    <p>
+                        {{ message['text'] }}
+                    </p>
+                    <p class="text-xs text-gray-700">
+                        {{ message['timestamp'] }}
+                    </p>
+                </div>
+                <div v-if="message['who']==='CPai'"
+                    class="flex flex-col items-end pr-4 py-1 self-end py-1 w-8/12 bg-blue-100 rounded"
+                    v-on:click="debugModal=!debugModal">
+                    <p class="font-semibold">
+                        {{ message['who'] }}
+                    </p>
+                    <p>
+                        {{ message['text'] }}
+                    </p>
+                    <p class="text-xs text-gray-700">
+                        {{ message['timestamp'] }}
+                    </p>
+                </div>
+            </div>
+        </section>
+        <form @submit.prevent="sendUtterance" class="mt-4 h-16 flex">
+            <label for="utterance" class="font-semibold"></label>
+            <textarea
+                type="text"
+                name="utterance"
+                class="resize-y border w-full rounded-lg py-2 px-4 outline-none text-xl"
+                placeholder="Enter your response"
+                required
+            />
+            <button
+                type="submit"
+                name="button"
+                class="ml-1 w-16 bg-blue-600 hover:bg-blue-500 text-center bg-gray-100 text-white font-semibold rounded-lg shadow-lg focus:outline-none"
+            >
+                Send
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+function keepScrollDown() {
+  const elem = document.getElementById("chat");
+  elem.scrollTop = elem.scrollHeight - elem.clientHeight;
+}
+
+export default {
+  name: "Chat",
+  updated() {
+    keepScrollDown();
+  },
+  data() {
+    return {
+      debugModal: false,
+      messages: [
+        {
+          id: 0,
+          who: "CPai",
+          text: "How may I help you?",
+          timestamp: Date.now()
+        }
+      ]
+    };
+  },
+  methods: {
+    sendUtterance(e) {
+      this.messages.push({
+        who: "You",
+        text: e.target.elements.utterance.value,
+        timestamp: Date.now()
+      });
+
+      const token = JSON.parse(localStorage.getItem("user"))["access_token"];
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      };
+      const data = {
+        query: e.target.elements.utterance.value,
+        language: "en",
+        device: "Web",
+        lat: 0,
+        lon: 0,
+        time_offset: 300,
+        ai_version: "83edcdeb-4803-4394-a2df-75fea771fc54"
+      };
+
+      console.log(headers); // eslint-disable-line no-console
+      axios({
+        method: "post",
+        url: "https://api.clinc.ai/v1/query",
+        headers,
+        data
+      })
+        .then(response => {
+          console.log(response); // eslint-disable-line no-console
+          this.messages.push({
+            who: "CPai",
+            text: response.data.visuals["formattedResponse"],
+            timestamp: response.data.today,
+            res: response.data
+          });
+        })
+        .catch(e => {
+          alert("error - see console msg.");
+          console.log(e); // eslint-disable-line no-console
+        });
+
+      e.target.elements.utterance.value = null;
+    }
+  }
+};
+</script>
+
+<style scoped>
+#chat {
+  min-height: 30%;
+  max-height: 70%;
+}
+</style>
--- frontend/tailwind.js
+++ frontend/tailwind.js
@@ -1,7 +1,14 @@
 module.exports = {
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+    theme: {
+        maxHeight: {
+            "0": "0",
+            "1/4": "25%",
+            "1/2": "50%",
+            "3/4": "75%",
+            full: "100%"
+        },
+        extend: {}
+    },
+    variants: {},
+    plugins: []
+};
