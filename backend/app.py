d72676431dcd28bb821b971f98fd90f7d1450d6d
harrydw <harrydw@umich.edu>
2020-02-16 18:59:34 -0500

    sample json

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request, jsonify
 import pyrebase
-import requests
-import json
 import pprint
 
 config = {
@@ -20,11 +18,41 @@ app = Flask(__name__)
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
+#@app.route('/explain_term', methods=['POST'])
+def explain_term():
+    # get payload
+    content = request.json
 
+    # for print debugging
+    pprint.pprint(content)
 
-@app.route('/')
+    # map it (in this case, the slot is mapped to the token)
+    extract = content['queryResult']['parameters']['terminology']
+    extract = standardize_token(extract)
+    firebase_data = db.child("TERMINOLOGY").get().val()
+    print(content['queryResult']['parameters']['term_definition'])
+    if extract not in firebase_data:
+        content['queryResult']['parameters']['term_definition'] = "Sorry, I don't think that's a relevant tax term"
+    else:
+        content['queryResult']['parameters']['term_definition'] = firebase_data[extract]
+
+    return jsonify(content)
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "Welcome to CPai!"
+    if request.method == 'POST':
+        # get payload
+        content = request.json
+
+        # for print debugging
+       # pprint.pprint(content)
+        intent = content['queryResult']['intent']['displayName']
+        print(intent)
+        if intent == 'explain_term':
+            return explain_term()
+
+    else:
+        return "Welcome to CPai!"
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -36,94 +64,23 @@ def login():
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
-@app.route('/user')
+@app.route('/new_user')
 def create_new_user():
-    print("Hello")
-    return "hello"
-
-@app.route('/explain_term', methods=['POST'])
-def star1t():
-    # maintain last city mentioned in last_city
-    global last_city
-
-    # get payload
     content = request.json
 
     # for print debugging
     pprint.pprint(content)
-    return jsonify(content)
+
+
+    print("Hello")
+    return "hello"
 
 def standardize_token(token):
     new_token = token.lower()
     return new_token.replace(" ", "_")
 
 
-@app.route('/', methods=['POST'])
-def start():
-    # get payload
-    content = request.json
 
-    # for print debugging
-    pprint.pprint(content)
-    # resolve the slot (if it's not resolved, the platform may complain in the response)
-    content['slots']['_TERMINOLOGY_']['values'][0]['resolved'] = 1
-
-    # map it (in this case, the slot is mapped to the token)
-    extract = content['slots']['_TERMINOLOGY_']['values'][0]['tokens']
-    extract = standardize_token(extract)
-    firebase_data = db.child("TERMINOLOGY").get().val()
-
-    if extract not in firebase_data:
-        content['slots']['_TERMINOLOGY_']['values'][0]['value'] = "Sorry, I don't think that's a relevant tax term"
-    else:
-        content['slots']['_TERMINOLOGY_']['values'][0]['value'] = firebase_data[extract]
-
-    return jsonify(content)
-
-
-'''
-def business_logic_explain_term():
-    if request.method == 'POST':
-        content = request.JSON
-        pprint.pprint(content)
-        # resolve the slot (if it's not resolved, the platform may complain in the response)
-        #content['slots']['_TERMINOLOGY_']['values'][0]['resolved'] = -1
-
-        # map it (in this case, the slot is mapped to the token)
-        #content['slots']['_TERMINOLOGY_']['values'][0]['tokens'] = 'see_vinod_it_works'
-        #content['slots']['_TERMINOLOGY_']['values'][0]['value'] = 'see_vinod_it_works'
-
-        #content['state'] = 'explain_instructions'
-
-
-        the_city = 'debbie_downer'
-        # resolve the slot (if it's not resolved, the platform may complain in the response)
-        content['slots']['_TERMINOLOGY_']['values'][0]['resolved'] = 1
-
-
-        # map it (in this case, the slot is mapped to the token)
-        content['slots']['_TERMINOLOGY_']['values'][0]['value'] = the_city
-        content['slots']['_TERMINOLOGY_']['values'][0]['tokens'] = 'bro_whatever'
-
-        return jsonify(content)
-'''
-@app.route('/term')
-def add_tax_terminology():
-    term = 'deduction'
-    def_attempt = retrieve_tax_definition(term).pyres
-    if def_attempt == None:
-        db.child('terminology').push({term: 'savings'})
-    else:
-        print("Term already in database")
-
-    return "Term " + term + " in database"
-
-def retrieve_tax_definition(term):
-    if db.child('terminology').get() == None:
-        print("Tax definition not in database")
-        return None
-    else:
-        return db.child('terminology').get()
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
