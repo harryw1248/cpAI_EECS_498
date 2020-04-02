653a189f1b35eaf36e54dc2b3cc08b135c0857cb
harrydw <harrydw@umich.edu>
2020-04-02 18:26:45 -0400

    trying to fix ngrock glitch

--- backend/document.py
+++ backend/document.py
@@ -304,6 +304,8 @@ class Document:
         for slot in self.deduction_slots_to_fill:
             if self.deduction_user_info[slot] is None:
                 return slot
+        
+        self.income_user_info["9"] = self.compute_line_9()
         return None
 
     def find_next_unfilled_slot_refund(self):
@@ -883,14 +885,18 @@ class Document:
             return amount
 
     def compute_line_9(self):
+        for key in self.deduction_user_info:
+            if self.deduction_user_info[key] is None:
+                self.deduction_user_info[key] = 0
+
         itemized_deductions = self.deduction_user_info['charitable-contributions'] +\
             self.deduction_user_info['state-local-taxes'] +\
             self.deduction_user_info['mortgage'] +\
             self.deduction_user_info['roth-IRA'] +\
             self.deduction_user_info['medical-dental-expenses'] +\
             self.deduction_user_info['jury-duty']
-        if self.income_user_info['9'] > itemized_deductions:
-            return self.income_user_info['9']
+        if self.income_user_info["9"] > itemized_deductions:
+            return self.income_user_info["9"]
         return itemized_deductions
         
         
