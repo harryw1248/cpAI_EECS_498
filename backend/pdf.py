5c90ee1da24338b9a4099f2920d0aa004a0c07d0
koeul <koeul@umich.edu>
2020-04-04 14:23:29 -0400

    fix space for dep name fields

--- backend/pdf.py
+++ backend/pdf.py
@@ -158,28 +158,28 @@ def fillInFields(document):
         fields["þÿc1_01[4]"]['check'] = True
 
     if len(document.dependents) > 0:
-        fields["þÿf1_14[0]"]['V'] = nonePipe(document.dependents[0].slots['dependent-given-name']) + '                                                      ' + nonePipe(document.dependents[0].slots['dependent-last-name'])
+        fields["þÿf1_14[0]"]['V'] = nonePipe(document.dependents[0].slots['dependent-given-name']) + '                                                  ' + nonePipe(document.dependents[0].slots['dependent-last-name'])
         fields["þÿf1_15[0]"]['V'] = nonePipe(document.dependents[0].slots['dependent-ssn'])
         fields["þÿf1_16[0]"]['V'] = nonePipe(document.dependents[0].slots['dependent-relation'])
         fields["þÿc1_12[0]"]['check'] = document.dependents[0].dependent_child_tax_credit     # child tax credit 1
         fields["þÿc1_13[0]"]['check'] = document.dependents[0].dependent_credit_for_others     # Credit for other dependents 1
 
     if len(document.dependents) > 1:
-        fields["þÿf1_17[0]"]['V'] = nonePipe(document.dependents[1].slots['dependent-given-name'])  + '                                                      ' + nonePipe(document.dependents[1].slots['dependent-last-name'])
+        fields["þÿf1_17[0]"]['V'] = nonePipe(document.dependents[1].slots['dependent-given-name'])  + '                                                  ' + nonePipe(document.dependents[1].slots['dependent-last-name'])
         fields["þÿf1_18[0]"]['V'] = nonePipe(document.dependents[1].slots['dependent-ssn'])
         fields["þÿf1_19[0]"]['V'] = nonePipe(document.dependents[1].slots['dependent-relation'] )
         fields["þÿc1_14[0]"]['check'] = document.dependents[1].dependent_child_tax_credit    # child tax credit 2
         fields["þÿc1_15[0]"]['check'] = document.dependents[1].dependent_credit_for_others    # Credit for other dependents 2
 
     if len(document.dependents) > 2:
-        fields["þÿf1_20[0]"][''] = nonePipe(document.dependents[2].slots['dependent-given-name']) + '                                                      ' + nonePipe(document.dependents[2].slots['dependent-last-name'])
+        fields["þÿf1_20[0]"][''] = nonePipe(document.dependents[2].slots['dependent-given-name']) + '                                                  ' + nonePipe(document.dependents[2].slots['dependent-last-name'])
         fields["þÿf1_21[0]"][''] = nonePipe(document.dependents[2].slots['dependent-ssn'])
         fields["þÿf1_22[0]"][''] = nonePipe(document.dependents[2].slots['dependent-relation'])
         fields["þÿc1_16[0]"]['check'] = document.dependents[2].dependent_child_tax_credit    # child tax credit 3
         fields["þÿc1_17[0]"]['check'] = document.dependents[2].dependent_credit_for_others    # Credit for other dependents 3
 
     if len(document.dependents) > 3:
-        fields["þÿf1_23[0]"][''] = nonePipe(document.dependents[3].slots['dependent-given-name']) + '                                                      ' + nonePipe(document.dependents[3].slots['dependent-last-name'])
+        fields["þÿf1_23[0]"][''] = nonePipe(document.dependents[3].slots['dependent-given-name']) + '                                                  ' + nonePipe(document.dependents[3].slots['dependent-last-name'])
         fields["þÿf1_24[0]"][''] = nonePipe(document.dependents[3].slots['dependent-ssn'])
         fields["þÿf1_25[0]"][''] = nonePipe(document.dependents[3].slots['dependent-relation'])
         fields["þÿc1_18[0]"]['check'] = document.dependents[3].dependent_child_tax_credit    # child tax credit 4
